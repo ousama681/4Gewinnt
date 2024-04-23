31f15f90ba93c8fce94d6d21f0c74205c80ef73d
Koneko <Koneko@KONEKO>
2024-04-23 20:31:33 +0200

    new glb file, css fixes, navbar fixes

--- wwwroot/js/main.js
+++ wwwroot/js/main.js
@@ -16,18 +16,18 @@ document.body.appendChild(renderer.domElement);
 
 const scene = new THREE.Scene();
 
-const camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 1, 1000);
-camera.position.set(4, 16, 11);
+const camera = new THREE.PerspectiveCamera(30, window.innerWidth / window.innerHeight, 1, 1000);
+camera.position.set(1, 1, 3);
 
 const controls = new OrbitControls(camera, renderer.domElement);
 controls.enableDamping = true;
 controls.enablePan = false;
-controls.minDistance = 3.2;
-controls.maxDistance = 20;
+controls.minDistance = 1.4;
+controls.maxDistance = 4;
 controls.minPolarAngle = 0.0;
 controls.maxPolarAngle = 1.5;
 controls.autoRotate = false;
-controls.target = new THREE.Vector3(0, 1, 0);
+controls.target = new THREE.Vector3(0, 0.2, 0);
 controls.update();
 
 const groundGeometry = new THREE.PlaneGeometry(20, 20, 32, 32);
@@ -41,14 +41,26 @@ groundMesh.castShadow = false;
 groundMesh.receiveShadow = true;
 scene.add(groundMesh);
 
-const spotLight = new THREE.SpotLight(0xffffff, 3, 100, 0.22, 1);
-spotLight.position.set(0, 25, 0);
+const spotLight = new THREE.SpotLight(0x0000fa, 1, 100, 0.22, 1);
+spotLight.position.set(0, 6, 0);
 spotLight.castShadow = true;
 spotLight.shadow.bias = -0.0001;
 scene.add(spotLight);
 
+const spotLight2 = new THREE.SpotLight(0xffffff, 8, 100, 0.22, 1);
+spotLight2.position.set(0, 3, 0);
+spotLight2.castShadow = true;
+spotLight2.shadow.bias = -0.0001;
+scene.add(spotLight2);
+
+const spotLight3 = new THREE.SpotLight(0x00ffff, 1, 100, 0.22, 1);
+spotLight3.position.set(0, 4.5, 0);
+spotLight3.castShadow = true;
+spotLight3.shadow.bias = -0.0001;
+scene.add(spotLight3);
+
 const loader = new GLTFLoader().setPath('/Assets/millennium_falcon/');
-loader.load('scene.gltf', (gltf) => {
+loader.load('RoboKing_3.glb', (gltf) => {
     const mesh = gltf.scene;
 
     mesh.traverse((child) => {
@@ -58,7 +70,7 @@ loader.load('scene.gltf', (gltf) => {
         }
     });
 
-    mesh.position.set(0, 1.05, -1);
+    mesh.position.set(0, 0.3, -0.35);
     scene.add(mesh);
 
     document.getElementById('progress-container').style.display = 'none';
