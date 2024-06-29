7fe329674feec591a42e98df0db69bd4e9b17966
Reading Steiner <ousama.askri01@gmail.com>
2024-06-29 23:16:18 +0200

    order descending

--- VierGewinnt/Controllers/HomeController.cs
+++ VierGewinnt/Controllers/HomeController.cs
@@ -177,7 +177,7 @@ namespace VierGewinnt.Controllers
         public IActionResult Leaderboard()
         {
             LeaderboardViewModel vm = new LeaderboardViewModel();
-            vm.PlayerRankings = _playerInfoRepo.GetAllAsync().Result.OrderBy(pr => pr.Wins);
+            vm.PlayerRankings = _playerInfoRepo.GetAllAsync().Result.OrderByDescending(pr => pr.Wins);
             return View(vm);
         }
 
